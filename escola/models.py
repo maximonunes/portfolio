0dde6c5e00e30179e9ca5a98ea3be4e7c2f74a4e
22408741-maximonunes <a22408741@alunos.ulht.pt>
2026-04-15 12:33:58 +0000

    Remoção do email na entidade de alunos

--- escola/models.py
+++ escola/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class Professor(models.Model):
     nome = models.CharField(max_length=100)
-    email = models.EmailField()
 
     def __str__(self):
         return self.nome
